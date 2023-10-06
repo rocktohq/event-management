14d6c26cc69422e25b53b83e7ad1520b923c40b9
Saidul Mursalin <itzmonir@gmail.com>
2023-10-07 01:41:53 +0600

    Title Added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,18 @@
 import Header from './../../components/Header/Header';
 import Banner from './../../components/Banner/Banner';
 import Footer from '../../components/Footer/Footer';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 const Home = () => {
   return (
-    <>
+    <HelmetProvider>
+      <Helmet>
+        <title>eManage - Home</title>
+      </Helmet>
       <Header></Header>
       <Banner></Banner>
       Home Page
       <Footer></Footer>
-    </>
+    </HelmetProvider>
   )
 }
 
