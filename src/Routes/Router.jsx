91dda38ed5c262d6734cba2ce9b14ecec707c38f
Saidul Mursalin <itzmonir@gmail.com>
2023-10-08 01:25:52 +0600

    Routers Updated

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -6,6 +6,9 @@ import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import Services from "../pages/Services/Services";
 import PrivateRoute from "./PrivateRoute";
+import AllServices from "../pages/AllServices/AllServices";
+import About from "../pages/About/About";
+import Contact from "../pages/Contact/Contact";
 
 const router = createBrowserRouter([
   {
@@ -24,12 +27,25 @@ const router = createBrowserRouter([
         loader: () => fetch("/services.json")
       },
       {
+        path: "/services",
+        element: <PrivateRoute><AllServices></AllServices></PrivateRoute>,
+        loader: () => fetch("/services.json")
+      },
+      {
         path: "/login",
         element: <Login></Login>
       },
       {
         path: "/register",
         element: <Register></Register>
+      },
+      {
+        path: "/about",
+        element: <About></About>
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>
       }
     ]
   }
