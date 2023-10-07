3e730e68b82b136c606ce75eddf045bf0ce28efc
Saidul Mursalin <itzmonir@gmail.com>
2023-10-08 05:36:17 +0600

    Progressbar Updated

--- src/pages/Home/Success.jsx
+++ src/pages/Home/Success.jsx
@@ -44,15 +44,15 @@ const Success = () => {
           <h2 className="text-2xl text-center md:text-left md:text-4xl lg:text-5xl font-bold bg-gradient-to-r from-indigo-700 to-purple-500 bg-clip-text text-transparent py-5">Explore our Management Services</h2>
           <p className="text-gray-500 mt-5">We ensure our services quality over quantity. Check out our success rate.</p>
         </div>
-        <div className="flex-1">
+        <div className="flex-1" data-aos="fade-down">
           <p className="text-xl font-medium text-gray-600">Professionality</p>
-          <ProgressBar completed="85" />
+          <ProgressBar completed="85" animateOnRender={true} />
           <p className="text-xl font-medium text-gray-600 mt-4">Food Ordering</p>
-          <ProgressBar completed="70" />
+          <ProgressBar completed="70" animateOnRender={true} />
           <p className="text-xl font-medium text-gray-600 mt-4">Experience and Quality</p>
-          <ProgressBar completed="90" />
+          <ProgressBar completed="90" animateOnRender={true} />
           <p className="text-xl font-medium text-gray-600 mt-4">Guarantee</p>
-          <ProgressBar completed="80" />
+          <ProgressBar completed="80" animateOnRender={true} />
         </div>
       </div>
     </>
