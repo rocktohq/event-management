94c8649db1796031731e52e68af67a6df1b0ddfa
Saidul Mursalin <itzmonir@gmail.com>
2023-10-07 06:31:51 +0600

    LocalStorage Functions Added

---
+++ src/utils/localStorage.js
@@ -0,0 +1,23 @@
+// Get Data from the Local Storage
+const getLocalStorageData = (option) => {
+  const localStorageData = localStorage.getItem(option);
+
+  if (localStorageData) {
+    return JSON.parse(localStorageData);
+  }
+  else {
+    return [];
+  }
+}
+
+// Save Data to the Local Storage
+const saveLocalStorageData = (id, option) => {
+  const localStorageData = getLocalStorageData();
+
+  if (localStorageData.includes(id)) return true;
+
+  localStorageData.push(id);
+  localStorage.setItem(option, JSON.stringify(localStorageData));
+}
+
+export { getLocalStorageData, saveLocalStorageData };
