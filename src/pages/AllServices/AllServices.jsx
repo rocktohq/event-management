c2abcb639a8d56c3fe40995148d30f0afab9abb5
Saidul Mursalin <itzmonir@gmail.com>
2023-10-08 01:24:56 +0600

    Services Updated

---
+++ src/pages/AllServices/AllServices.jsx
@@ -0,0 +1,42 @@
+import { Helmet, HelmetProvider } from "react-helmet-async"
+import Footer from "../../components/Footer/Footer"
+import Header from "../../components/Header/Header"
+import { useLoaderData } from "react-router-dom"
+import ServiceCard from "../Shared/ServiceCard/ServiceCard"
+import { useState } from "react"
+
+const AllServices = () => {
+
+  const [showServices, setShowServices] = useState(8);
+  const services = useLoaderData();
+  console.log(services)
+
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>All Services</title>
+      </Helmet>
+      <Header></Header>
+      <main>
+        {/* All Services */}
+        <section className="py-12">
+          <div className="max-w-screen-xl mx-auto px-3">
+            <h2 className="divider text-3xl md:text-4xl lg:text-5xl font-bold text-center bg-gradient-to-r from-indigo-700 to-purple-500 bg-clip-text text-transparent py-10">Our Services</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+              {
+                services?.slice(0, showServices).map(service => <ServiceCard key={service.id} service={service}></ServiceCard>)
+
+              }
+            </div>
+            <div className={`text-center mt-10 ${showServices >= services.length && "hidden"}`}>
+              <button onClick={() => setShowServices(services.length)} className="btn btn-primary rounded">Show All</button>
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer></Footer>
+    </HelmetProvider>
+  )
+}
+
+export default AllServices
--- src/pages/Services/Service.jsx
+++ src/pages/Services/Service.jsx
@@ -1,16 +1,23 @@
 import PropTypes from 'prop-types'
 import { FaCartArrowDown, FaHeart } from 'react-icons/fa';
+import { saveLocalStorageData } from '../../utils/localStorage';
+import toast from 'react-hot-toast';
 
 const Service = ({ service }) => {
 
-  const { title, image, price, longDescription } = service;
+  const { id, title, image, price, longDescription } = service;
 
-  const handleAddBtn = (option) => {
-    console.log(option);
+  const handleAddBtn = (id, option) => {
+    console.log(option, id);
+
+    // Save to LocalStorage
+    const save = saveLocalStorageData(id, option);
+    save ? toast.error(`Service already exists in your ${option}`) : toast.success(`Service added to your ${option}`);
+
   }
 
   return (
-    <section className="my-12 md:my-16 lg:my-20">
+    <section className="mb-12 md:mb-16 lg:mb-20">
       <div className="flex flex-col lg:flex-row gap-10">
         <figure className="w-full h-full flex-1">
           <img className="w-full h-full rounded-md" src={image} alt={title} />
@@ -18,10 +25,10 @@ const Service = ({ service }) => {
         <div className="space-y-5 flex-1">
           <h3 className="text-2xl font-bold">{title}</h3>
           <p className="text-gray-500">{longDescription}</p>
-          <p>Price: ${price}</p>
+          <p className="bg-cyan-100 rounded px-5 py-2  w-fit">Price: ${price}</p>
           <div className="flex flex-col lg:flex-row gap-5">
-            <button onClick={() => handleAddBtn("wishlist")} className="btn btn-primary rounded flex-1"><FaHeart /> Add to Wishlist</button>
-            <button onClick={() => handleAddBtn("cart")} className="btn btn-primary rounded flex-1"><FaCartArrowDown /> Add to Cart</button>
+            <button onClick={() => handleAddBtn(id, "wishlist")} className="btn btn-primary rounded flex-1"><FaHeart /> Add to Wishlist</button>
+            <button onClick={() => handleAddBtn(id, "cart")} className="btn btn-primary rounded flex-1"><FaCartArrowDown /> Add to Cart</button>
           </div>
         </div>
       </div>
