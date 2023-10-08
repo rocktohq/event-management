ffd60b71c11cf3961e1eb3b0faedf195f9aa10f1
Saidul Mursalin <itzmonir@gmail.com>
2023-10-08 22:49:00 +0600

    Navbar Updatdd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-1">
             {navLinks}
           </ul>
         </div>
@@ -55,7 +55,7 @@ const Navbar = () => {
         <span className="text-4xl text-neutral font-bold ml-2 hidden md:block">Manage</span>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-lg">
+        <ul className="menu menu-horizontal px-1 text-lg space-x-2">
           {navLinks}
         </ul>
       </div>
@@ -68,7 +68,7 @@ const Navbar = () => {
                 <img src={user.photoURL} alt="" />
               </div>
             </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-md w-52 text-xl">
+            <ul tabIndex={0} className="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-md w-52 text-xl">
               {profilLinks}
             </ul>
           </div>
