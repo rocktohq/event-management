9154b4a506bbf6244ec40ca569f540900db1918e
Saidul Mursalin <itzmonir@gmail.com>
2023-10-08 22:52:48 +0600

    Navbar Updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = () => {
   // Nav Links
   const navLinks = <>
     <li><NavLink to="/">Home</NavLink></li>
-    <li><NavLink to="/about">About</NavLink></li>
     <li><NavLink to="/services">Services</NavLink></li>
+    <li><NavLink to="/about">About</NavLink></li>
     <li><NavLink to="/contact">Contact</NavLink></li>
   </>
 
