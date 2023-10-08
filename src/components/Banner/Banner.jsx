653d81dd27b7e218ba56531a6631e460f3148e84
Saidul Mursalin <itzmonir@gmail.com>
2023-10-08 23:02:15 +0600

    Banner Updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,8 +6,8 @@ const Banner = () => {
       <div className="max-w-screen-xl mx-auto px-3">
         <div className="flex flex-col-reverse lg:flex-row justify-center items-center gap-10">
           <div className="flex-1">
-            <h1 className="text-2xl text-center md:text-left md:text-4xl lg:text-7xl font-bold bg-gradient-to-r from-indigo-700 to-purple-500 bg-clip-text text-transparent">{`Let's Plan Your Next Event Together`}</h1>
-            <p className="mt-5 text-justify text-gray-500">
+            <h1 className="text-2xl text-center md:text-left md:text-4xl lg:text-7xl font-bold bg-gradient-to-r from-indigo-700 to-purple-500 bg-clip-text text-transparent pb-5">{`Let's Plan Your Next Event Together`}</h1>
+            <p className="text-justify text-gray-500">
               Discover the art of exceptional event planning at <span className="text-primary">eManage</span>. From enchanting weddings and joyful birthdays to meaningful retirements, we specialize in creating unforgettable moments tailored to your unique vision. Our dedicated team, crafts seamless experiences infused with your personal touch.
             </p>
           </div>
