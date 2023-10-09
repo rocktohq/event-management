d179ba713b5611e31df57321d1c1f9362c29ff14
Saidul Mursalin <itzmonir@gmail.com>
2023-10-09 17:01:40 +0600

    Features Added

--- src/pages/Services/Service.jsx
+++ src/pages/Services/Service.jsx
@@ -2,10 +2,11 @@ import PropTypes from 'prop-types'
 import { FaCartArrowDown, FaHeart } from 'react-icons/fa';
 import { saveLocalStorageData } from '../../utils/localStorage';
 import toast from 'react-hot-toast';
+import { TiTick } from "react-icons/ti";
 
 const Service = ({ service }) => {
 
-  const { id, title, image, price, longDescription } = service;
+  const { id, title, image, price, longDescription, features } = service;
 
   const handleAddBtn = (id, option) => {
 
@@ -18,13 +19,19 @@ const Service = ({ service }) => {
   return (
     <section className="mb-12 md:mb-16 lg:mb-20">
       <div className="flex flex-col lg:flex-row gap-10">
-        <figure className="w-full h-full flex-1">
+        <figure className="flex-1">
           <img className="w-full h-full rounded-md" src={image} alt={title} />
         </figure>
         <div className="space-y-5 flex-1">
           <h3 className="text-2xl font-bold">{title}</h3>
           <p className="text-gray-500">{longDescription}</p>
-          <p className="bg-cyan-100 rounded px-5 py-2 w-fit font-bold">Price: ${price}</p>
+          <ul className="space-y-1 text-gray-500">
+            <p className="font-semibold mb-2 text-gray-700 text-xl">Features:</p>
+            {
+              features && features.map((feature, idx) => <li key={idx} className="flex gap-1"><TiTick /> <span>{feature}</span></li>)
+            }
+          </ul>
+          <p className="bg-cyan-100 rounded px-5 py-2 w-fit mb-3 text-gray-700">Price: $<span className="text-gray-500 font-bold text-2xl">{price}</span></p>
           <div className="flex flex-col lg:flex-row gap-5">
             <button onClick={() => handleAddBtn(id, "wishlist")} className="btn btn-primary rounded flex-1"><FaHeart /> Add to Wishlist</button>
             <button onClick={() => handleAddBtn(id, "cart")} className="btn btn-primary rounded flex-1"><FaCartArrowDown /> Add to Cart</button>
--- src/pages/Shared/ServiceCard/ServiceCard.jsx
+++ src/pages/Shared/ServiceCard/ServiceCard.jsx
@@ -3,9 +3,10 @@ import 'aos/dist/aos.css';
 import PropTypes from 'prop-types'
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { TiTick } from "react-icons/ti";
 
 const ServiceCard = ({ service }) => {
-  const { id, title, image, price, deal, shortDescription } = service;
+  const { id, title, image, price, deal, features } = service;
 
 
   // AOS Init
@@ -14,7 +15,7 @@ const ServiceCard = ({ service }) => {
       offset: 100,
       duration: 500,
       easing: 'ease-in-sine',
-      once:true,
+      once: true,
     });
   }, [])
 
@@ -29,11 +30,15 @@ const ServiceCard = ({ service }) => {
       <div className="p-5">
         <div className="card-body p-0">
           <h2 className="font-bold">{title}</h2>
-          <p className="text-gray-500 text-justify">{shortDescription}</p>
-          <p className="bg-cyan-100 rounded px-5 py-2  w-fit">Price: <span className="text-gray-500 font-bold">${price}</span></p>
+          <ul className="space-y-1 text-gray-500">
+            {
+              features && features.map((feature, idx) => <li key={idx} className="flex gap-1"><TiTick /> <span>{feature}</span></li>)
+            }
+          </ul>
         </div>
       </div>
       <div className="p-5">
+        <p className="bg-cyan-100 rounded px-5 py-2 w-fit mb-3 text-gray-700">Price: $<span className="text-gray-500 font-bold text-2xl">{price}</span></p>
         <Link to={`/service/${id}`}><button className="btn btn-primary rounded w-full">View Details</button></Link>
       </div>
       {
